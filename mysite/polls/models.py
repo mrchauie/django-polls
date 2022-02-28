00db2a10eec55636eacb6c434bed6ee5838eb887
mrchauie <willchau@gmail.com>
2022-02-28 13:29:10 +0800

    updated the details page to include header.html

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -23,8 +23,6 @@ class Question(models.Model):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 
-
-
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
@@ -32,3 +30,4 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
