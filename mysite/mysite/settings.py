431915f78bdc4c6b15d1a909b4b8c42242635fef
mrchauie <willchau@gmail.com>
2022-03-08 17:06:41 +0800

    added heroku settings for deployment

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import django_heroku
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,3 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = '/polls/'
 LOGOUT_REDIRECT_URL = '/polls/'
 ACCOUNT_SIGNUP_REDIRECT_URL = '/polls/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
